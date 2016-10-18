4bd04e6cc36f0fc9d74f45dbaa5c3b8bae491c6e
Doyeon Kim <ehdus859599@gmail.com>
2016-10-18 19:29:37 +0900

    #4 Implement readDiary method

--- app/src/main/java/kr/hs/emirim/dok96/simplediary/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dok96/simplediary/MainActivity.java
@@ -1,13 +1,65 @@
 package kr.hs.emirim.dok96.simplediary;
 
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    DatePicker datePic;
+    EditText editDiary;
+    Button butSave;
+    String fileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        datePic=(DatePicker)findViewById(R.id.date_picker);
+        editDiary=(EditText) findViewById(R.id.edit_content);
+        butSave=(Button) findViewById(R.id.but_save);
+
+        //현재 날짜 구하기
+        Calendar calendar=Calendar.getInstance();
+        int nowYear = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH)+1;
+        int date = calendar.get(Calendar.DATE);
+
+        //DatePicker에 현재 날짜 설정
+        datePic.init(nowYear, month, date, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                fileName = year + "_" + (monthOfYear + 1) + "_" + dayOfMonth + ".txt";
+                String content = readDiary(fileName);
+                editDiary.setText(content);
+                butSave.setEnabled(true);
+
+            }
+        });
+    }
+    String readDiary(String fileName) {
+
+        String diaryContents=null;
+        try {
+            FileInputStream in= openFileInput(fileName);
+            byte[] txt=new byte[500];
+            in.read(txt);
+            in.close();
+            diaryContents=new String(txt);
+            butSave.setText("수정하기");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+           editDiary.setHint("읽어올 일기가 없음");
+            butSave.setText("새로 저장");
+
+        }
+        return  diaryContents;
     }
 }
